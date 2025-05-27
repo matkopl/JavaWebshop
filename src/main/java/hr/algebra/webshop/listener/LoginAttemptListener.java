d93729ac6544e7cb235f890a2d37faba3ccb4e42
matkopl <mpleca6@algebra.hr>
2025-05-27 14:55:29 +0200

    lil fix

--- src/main/java/hr/algebra/webshop/listener/LoginAttemptListener.java
+++ src/main/java/hr/algebra/webshop/listener/LoginAttemptListener.java
@@ -1,6 +1,7 @@
 package hr.algebra.webshop.listener;
 
 import hr.algebra.webshop.service.LoginHistoryService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AuthenticationFailureBadCredentialsEvent;
@@ -8,6 +9,8 @@ import org.springframework.security.authentication.event.AuthenticationSuccessEv
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Component
 @RequiredArgsConstructor
@@ -17,19 +20,30 @@ public class LoginAttemptListener {
 
     @EventListener
     private void handleSuccess(AuthenticationSuccessEvent successEvent) {
+        String ip = getClientIpAddress();
         Authentication authentication = successEvent.getAuthentication();
-        if (authentication.getDetails() instanceof WebAuthenticationDetails details) {
-            loginHistoryService.logLogin(authentication.getName(), details.getRemoteAddress(), "SUCCESS");
-        }
+        loginHistoryService.logLogin(authentication.getName(), ip, "SUCCESS");
     }
 
     @EventListener
     private void handleFailure(AuthenticationFailureBadCredentialsEvent failureEvent) {
         Object principal = failureEvent.getAuthentication().getPrincipal();
         String username = principal instanceof String ? (String) principal : "UNKNOWN USER";
-        Object details = failureEvent.getAuthentication().getDetails();
-        String ip = details instanceof WebAuthenticationDetails ? ((WebAuthenticationDetails) details).getRemoteAddress() : "UNKNOWN IP";
+        String ip = getClientIpAddress();
         loginHistoryService.logLogin(username, ip, "FAILURE");
+    }
+
+
+    private String getClientIpAddress() {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) return "UNKNOWN IP";
+
+        HttpServletRequest request = attributes.getRequest();
+        String xfHeader = request.getHeader("X-Forwarded-For");
+        if (xfHeader != null && !xfHeader.isEmpty()) {
+            return xfHeader.split(",")[0].trim();
+        }
+        return request.getRemoteAddr();
     }
 
 }
